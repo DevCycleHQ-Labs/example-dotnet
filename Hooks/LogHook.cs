b5c8a943dba0e7364eaeffef70a461142e352ae8
phaym <1431593+phaym@users.noreply.github.com>
2025-09-04 16:15:40 -0400

    fix attributes not showing because of dictionary definition

--- Hooks/LogHook.cs
+++ Hooks/LogHook.cs
@@ -13,58 +13,30 @@ namespace HelloTogglebot.Hooks
 
         public override Task ErrorAsync<T>(HookContext<T> context, System.Exception error, CancellationToken cancellationToken = default)
         {
-            var attributes = new Dictionary<string, object>
-            {
-                ["feature_flag.key"] = context.Key,
-                ["feature_flag.provider.name"] = "devcycle",
-                ["feature_flag.context.id"] = context.User.UserId,
-                ["feature_flag.value_type"] = context.DefaultValue.GetType().Name,
-                ["feature_flag.error_message"] = error.Message,
-                ["error.type"] = error.GetType().Name,
-            };
+            var attributes = SetContextAttributes<T>(context);
+            attributes.Add("feature_flag.error_message", error.Message);
+            attributes.Add("error.type", error.GetType().Name);
 
-            if (context.Metadata != null)
-            {
-                attributes["feature_flag.project"] = context.Metadata.Project?.Id;
-                attributes["feature_flag.environment"] = context.Metadata.Environment?.Id;
-            }
             using (_logger.BeginScope(attributes))
             {
-                _logger.LogError($"Error evaluating: {context.Key}");
+                _logger.LogError($"Error evaluating flag: {context.Key}");
             }
             return Task.CompletedTask;
         }
 
-        public override Task FinallyAsync<T>(HookContext<T> context, Variable<T> variableDetails, VariableMetadata variableMetadata, CancellationToken cancellationToken = default)
+        public override Task AfterAsync<T>(HookContext<T> context, Variable<T> variableDetails, VariableMetadata variableMetadata, CancellationToken cancellationToken = default)
         {
-            var attributes = new Dictionary<string, object>
-            {
-                ["feature_flag.key"] = context.Key,
-                ["feature_flag.provider.name"] = "devcycle",
-                ["feature_flag.context.id"] = context.User.UserId,
-                ["feature_flag.value_type"] = context.DefaultValue.GetType().Name,
-                ["feature_flag.result.value"] = variableDetails.Value?.ToString()
-            };
-
-            // Add metadata if available
-            if (context.Metadata != null)
-            {
-                attributes["feature_flag.project"] = context.Metadata.Project?.Id;
-                attributes["feature_flag.environment"] = context.Metadata.Environment?.Id;
-            }
-
-            // Add feature-specific attributes if available
+            var attributes = SetContextAttributes<T>(context);
+            attributes.Add("feature_flag.result.value", variableDetails.Value.ToString() ?? "");
             if (variableMetadata.FeatureId != null)
             {
-                attributes["feature_flag.set.id"] = variableMetadata.FeatureId;
-                attributes["feature_flag.url"] = $"https://app.devcycle.com/r/p/{context.Metadata.Project.Id}/f/{variableMetadata.FeatureId}";
+                attributes.Add("feature_flag.set.id", variableMetadata.FeatureId);
+                attributes.Add("feature_flag.url", $"https://app.devcycle.com/r/p/{context.Metadata?.Project.Id}/f/{variableMetadata.FeatureId}");
             }
-
-            // Add evaluation details if available
             if (variableDetails.Eval != null)
             {
-                attributes["feature_flag.result.reason"] = variableDetails.Eval.Reason;
-                attributes["feature_flag.result.reason.details"] = variableDetails.Eval.Details;
+                attributes.Add("feature_flag.result.reason", variableDetails.Eval.Reason);
+                attributes.Add("feature_flag.result.reason.details", variableDetails.Eval.Details);
             }
 
             using (_logger.BeginScope(attributes))
@@ -72,6 +44,26 @@ namespace HelloTogglebot.Hooks
                 _logger.LogInformation($"Feature flag evaluated {context.Key}");
             }
             return Task.CompletedTask;
+        }
+
+        private IDictionary<string, object> SetContextAttributes<T>(HookContext<T> context)
+        {
+            var attributes = new Dictionary<string, object>
+            {
+                ["feature_flag.key"] = context.Key,
+                ["feature_flag.provider.name"] = "devcycle",
+                ["feature_flag.context.id"] = context.User.UserId,
+            };
+            if (context.DefaultValue != null)
+            {
+                attributes.Add("feature_flag.value_type", context.DefaultValue.GetType().Name);
+            }
+            if (context.Metadata != null)
+            {
+                attributes.Add("feature_flag.project", context.Metadata.Project.Id);
+                attributes.Add("feature_flag.environment", context.Metadata.Environment.Id);
+            }
+            return attributes;
         }
     }
 }
